56474ce8b2b1cd5f647721dd892e483ab0214cc3
KruJeen <44271005+krujeen@users.noreply.github.com>
2019-10-16 12:02:37 +0700

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main() 
+{
+  int n;
+  cin>>n;
+  int fibo[n];
+  fibo[0]=0;
+  fibo[1]=1;
+  fibo[2]=1;
+  for(int i=3;i<n;i++)
+  {
+    fibo[i]=fibo[i-1]+fibo[i-2];
+  }
+  for(int i=0;i<n;i++)
+  {
+    cout<<fibo[i];
+    if(i!=n) cout<<' ';
+  }
+  return 0;
+}
