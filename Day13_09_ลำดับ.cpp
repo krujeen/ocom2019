1670593b27b67e373e0de8f794d87eb01931576a
KruJeen <44271005+krujeen@users.noreply.github.com>
2019-10-19 09:47:34 +0700

    Create Day13_09_ลำดับ.cpp

---
+++ Day13_09_ลำดับ.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int n;
+  cin>>n;
+  for(int i=3;i<n;i+=5)
+  {
+    cout<<i<<",";
+  }
+  cout<<n;
+  return 0;
+  
+  
+  return 0;
+}
